6d0580a8856b32ad7957aa2185f52b6b918c47f3
Kivorob <71591773+Kivorob@users.noreply.github.com>
2022-04-26 23:59:53 +1000

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import InfoBar from "./components/Layout/Footer/InfoBar/InfoBar";
 
 function App() {
   return (
     <div>
-
+          <InfoBar/>
     </div>
   );
 }
