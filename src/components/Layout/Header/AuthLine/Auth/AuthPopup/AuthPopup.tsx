a05eaf70871a5915dcd7d2d58b7ff6aeb9f3ab74
Kivorob <71591773+Kivorob@users.noreply.github.com>
2022-04-27 15:56:57 +1000

    Added login func in site

--- src/components/Layout/Header/AuthLine/Auth/AuthPopup/AuthPopup.tsx
+++ src/components/Layout/Header/AuthLine/Auth/AuthPopup/AuthPopup.tsx
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import style from './AuthPopup.module.scss';
 
-const AuthPopup = () => {
+
+async function loginUser(data?: any) {
+	return fetch('http://localhost:8000/api/login', {
+		method: 'POST',
+		body: JSON.stringify(data),
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	}).then((res => res.json))
+}
+
+function AuthPopup ({auth, setAuthorised, user, setUser}: {auth?: any, setAuthorised?: any, user?: any, setUser?: any}) {
+	const handleSubmit = async (event?: any) => {
+		event.preventDefault();
+		console.log(event);
+		const data = {
+			telephone: event.target.elements.telephone.value,
+			password: event.target.elements.password.value
+		}
+		console.log(data);
+		let token: any;
+		token = await loginUser(data).catch(err => token = {token: 'error'});
+		if (token.token !== 'error') {
+			console.log(token);
+			auth = true;
+			setAuthorised(auth);
+			user = token.user;
+			setUser(user);
+			localStorage.setItem('token', token.token);
+			console.log(localStorage.getItem("token"));
+		}
+	}
+
 	return (
-		<form action="#" className={style.container}>
+		<form onSubmit={handleSubmit} className={style.container}>
 			<p className={style.title}>Вход</p>
 			<div className={style.input}>
-				<input type="text" id="login" placeholder="Номер телефона"/>
+				<input type="text" id="telephone" placeholder="Номер телефона"/>
 				<input type="password" id="password" placeholder="Пароль"/>
 				<button type="submit">Вход</button>
 			</div>
@@ -14,7 +46,7 @@ const AuthPopup = () => {
 				<a href="#">Регистрация</a>
 			</div>
 		</form>
-	);
-};
+	)
+}
 
 export default AuthPopup;
